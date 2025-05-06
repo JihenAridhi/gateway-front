144081c3595c56892e048fb20fce2fc353aa651b
JihenAridhi <aridhijihen1@gmail.com>
2025-05-06 09:40:53 +0100

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersComponent } from './components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'account', component: SideNavComponent, children:
+        [
+          {path: 'dashboard', component: DashboardComponent},
+          {path: 'users', component: UsersComponent},
+          //{path: 'transactions', component: },
+        ]
+    },
+];
